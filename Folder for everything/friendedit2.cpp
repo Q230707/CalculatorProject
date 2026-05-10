b9e92f2950c1b0b9c88d9ad068cff01bf7a6c274
Quna_Tap_Code <ho8891975@gmail.com>
2026-05-10 09:10:55 +0000

    Add more sign and give everything in folder

---
+++ Folder for everything/friendedit2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=4,*b=&a;
+    cout<<&a<<endl;
+    cout<<b<<endl;
+    if(b==&a)cout<<"You are good!"<<endl;
+    cout << 2 + 3 << endl;
+    cout << 5 - 2;
+}
